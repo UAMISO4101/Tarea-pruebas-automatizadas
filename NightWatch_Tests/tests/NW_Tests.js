51bb875a1cd8291c835af684976f31c85aa4bdd1
camilofg <rc.forero@uniandes.edu.co>
2017-08-25 00:58:28 -0500

    nightwatch funcionando
    nightwatch funcionando

--- NightWatch_Tests/tests/NW_Tests.js
+++ NightWatch_Tests/tests/NW_Tests.js
@@ -1,29 +1,16 @@
 module.exports = { // adapted from: https://git.io/vodU0
-  // 'Los estudiantes login falied': function(browser) {
-  //   browser
-  //     .url('https://losestudiantes.co/')
-  //     .click('.botonCerrar')
-  //     .waitForElementVisible('.botonIngresar', 4000)
-  //     .click('.botonIngresar')
-  //     .setValue('.cajaLogIn input[name="correo"]', 'wrongemail@example.com')
-  //     .setValue('.cajaLogIn input[name="password"]', '1234')
-  //     .click('.cajaLogIn .logInButton')
-  //     .waitForElementVisible('.aviso.alert.alert-danger', 4000)
-  //     .assert.containsText('.aviso.alert.alert-danger', 'El correo y la contraseña que ingresaste no figuran')
-  //     .end();
-  // },
-
-  'Los estudiantes login succesfully': function(browser) {
+  'Los estudiantes login falied': function(browser) {
     browser
       .url('https://losestudiantes.co/')
       .click('.botonCerrar')
       .waitForElementVisible('.botonIngresar', 4000)
       .click('.botonIngresar')
-      .setValue('.cajaLogIn input[name="correo"]', 'camus375@hotmail.com')
-      .setValue('.cajaLogIn input[name="password"]', 'Qwerty123')
+      .waitForElementVisible('..cajaLogIn input[name="correo"]', 4000)
+      .setValue('.cajaLogIn input[name="correo"]', 'wrongemail@example.com')
+      .setValue('.cajaLogIn input[name="password"]', '1234')
       .click('.cajaLogIn .logInButton')
-      .waitForElementVisible('.usrImage.fa.fa-user-circle.fa-2x', 4000)
-      //.assert.containsText('.usrImage.fa.fa-user-circle.fa-2x', '')
+      .waitForElementVisible('.aviso.alert.alert-danger', 4000)
+      .assert.containsText('.aviso.alert.alert-danger', 'El correo y la contraseña que ingresaste no figuran')
       .end();
   }
 };
